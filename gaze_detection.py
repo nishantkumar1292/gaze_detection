9c1ef658f99a20de2d1ebf6dafc379f22197512f
Nishant <nishantk@dewsolutions.in>
2018-09-14 20:00:29 +0530

    dynamic functionality for url handling

--- gaze_detection.py
+++ gaze_detection.py
@@ -1,44 +1,114 @@
 import cv2
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import urllib
+import os
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eyes_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-def get_video(video_url):
+def run_for_video(video_url):
 	video_path = download_video(video_url)
+	response = analyse_video(video_path)
+	return response
+
+def download_video(url):
+	directory = 'videos'
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	video = url.split("/")[-1]
+	video_path = os.path.join(directory, video)
+	if not os.path.exists(video_path):
+		urllib.urlretrieve (url, video_path)
 	return video_path
 
 def analyse_video(video_path):
-	distances, angles = get_gaze_points(video_path)
-	gaze_analysis = get_gaze_analysis(distances, angles)
+	distances, angles, video_frames, face_frames, eye_frames = get_gaze_points(video_path)
+	external_help_score = get_gaze_analysis(distances, angles)
+	face_confidence, eye_confidence = (face_frames*1.0)/video_frames, (eye_frames*1.0)/video_frames
+	return {"face_confidence": face_confidence, "eye_confidence": eye_confidence, "score": external_help_score}
 
 
 def get_gaze_points(video_path):
 	distances = []
 	angles = []
 	cap = cv2.VideoCapture(video_path)
+	video_frames = 0
+	face_frames = 0
+	eye_frames = 0
 	while cap.isOpened():
 		ret, img = cap.read()
+		if img is None:
+			cap.release()
+			break
+		video_frames += 1
 		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 		faces = face_cascade.detectMultiScale(gray)
 		for (x,y,w,h) in faces:
+			face_frames += 1
 			face_gray = gray[y:y+h, x:x+w]
 			eyes = eyes_cascade.detectMultiScale(face_gray)
 			for (ex,ey,ew,eh) in eyes:
+				eye_frames += 0.5
 				eye_center = (ex+ew/2,ey+eh/2)
 				eyes_gray = face_gray[ey:ey+eh, ex:ex+ew]
 				(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(eyes_gray) #minLoc is the location for minimumm intensity
-				print("eye_center", (ew/2, eh/2))
-				print("eye_ball", minLoc)
+				# print("eye_center", (ew/2, eh/2))
+				# print("eye_ball", minLoc)
 
 				#calculating distance and angle
 				distance = math.sqrt(math.pow(ew/2-minLoc[0],2) + math.pow(eh/2-minLoc[1],2))
 				angle = math.degrees(math.atan2(minLoc[1]-eh/2, minLoc[0]-ew/2))
 				distances.append(distance)
 				angles.append(angle)
-	return distances, angles
+				if len(distances)%10==0:
+					print("{} frames done".format(len(distances)))
+	return distances, angles, video_frames, face_frames, eye_frames
 
-def get_gaze_analysis():
-	pass
+def get_gaze_analysis(distances, angles):
+	#constriants
+	threshold_distance = 10
 
-analyse_video(0)
+	#analysis
+	np_distances = np.array(distances)
+	np_angles = np.array(angles)
+	df = pd.DataFrame({'distance': np_distances, 'angle': np_angles})
+	score = get_score(df, threshold_distance)
+	return score
+
+def get_score(df, threshold_distance):
+	total_row_count = df.shape[0]
+	filtered_row_count = df[df['distance']>threshold_distance].shape[0]
+	if (filtered_row_count*1.0)/total_row_count<0.3:
+		return 0.0
+	else:
+		df['label'] = df.apply(get_label, axis=1)
+		grouped_count = df[df['distance']>threshold_distance].groupby(['label']).size().reset_index(name='counts').sort_values(by=['counts']).shape[0]
+		return (((8-grouped_count)*1.0)/8)*100
+
+
+
+def get_label(row):
+	if 0<=row['angle']<45 or -360<=row['angle']<-315:
+		return '0-45'
+	elif 45<=row['angle']<90 or -315<=row['angle']<-270:
+		return '45-90'
+	elif 90<=row['angle']<135 or -270<=row['angle']<-225:
+		return '90-135'
+	elif 135<=row['angle']<180 or -225<=row['angle']<-180:
+		return '135-180'
+	elif 180<=row['angle']<225 or -180<=row['angle']<-135:
+		return '180-225'
+	elif 225<=row['angle']<270 or -135<=row['angle']<-90:
+		return '225-270'
+	elif 270<=row['angle']<315 or -90<=row['angle']<-45:
+		return '270-315'
+	elif 315<=row['angle']<360 or -45<=row['angle']<0:
+		return '315-360'
+	else:
+		return "NA"
+
+run_for_video('https://s3.ap-south-1.amazonaws.com/fjinterviewanswersmumbai/209115a3d1a1b81f3620171222201339.mp4')
+# analyse_video(0)
--- server.py
+++ server.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
+#file imports
+from gaze_detection import run_for_video
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,17 +13,8 @@ def check():
 
 @app.route('/gaze_detect')
 def get_gaze_detection():
-	video = request.args.get('video')
-	if video not in ["sample1", "sample2", "sample3"]:
-		return jsonify({"error": "video not found"})
-	elif video == "sample1":
-		result = {"external_help_score": 38.6, "score_confidence": 92, "score_result": "cheating", "video": video, "directions":{"center": 60.5, "top": 30.2, "top_right":0, "right": 8.4, "right_bottom": 0, "bottom": 0, "bottom_left": 0.4, "left": 0.5, "left_top": 0} }
-	elif video == "sample2":
-		result = {"external_help_score": 21.2, "score_confidence": 93, "score_result": "not cheating", "video": video, "directions":{"center": 53.5, "top": 10.5, "top_right":0, "right": 10.8, "right_bottom": 1.7, "bottom": 1.5, "bottom_left": 1.4, "left": 10.7, "left_top": 0} }
-	elif video == "sample3":
-		result = {"external_help_score": 19.2, "score_confidence": 92, "score_result": "not cheating", "video": video, "directions":{"center": 70.4, "top": 12.6, "top_right": 6.6, "right": 3.5, "right_bottom": 0, "bottom": 0, "bottom_left": 0.3, "left": 6.2, "left_top": 0.5} }
-	else:
-		result = {"error": "some internal error"}
+	video_url = request.args.get('url')
+	result =  run_for_video(url)
 	return jsonify(result)
 
 if __name__ == '__main__':
