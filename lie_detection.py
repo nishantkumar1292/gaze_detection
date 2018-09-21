4cf4e8312ce56c064515d8e79b136b9546ef94d5
Nishant <nishantk@dewsolutions.in>
2018-09-21 15:21:25 +0530

    add path

--- lie_detection.py
+++ lie_detection.py
@@ -5,7 +5,9 @@ import json
 def get_lie_detection(video_path):
 	current_path = os.path.dirname(os.path.abspath(__file__))
 	abs_video_path = os.path.join(current_path, video_path)
-	subprocess.call("/home/ubuntu/vdohireai/bin/lie_detection.sh {}".format(abs_video_path))
+	command = "/home/ubuntu/vdohireai/bin/lie_detection.sh {}".format(abs_video_path)
+	print(command)
+	subprocess.call(command, shell=True)
 	output_path = "/home/ubuntu/vdohireai/response.txt"
 	response = {}
 	with open(output_path, 'r') as file:
